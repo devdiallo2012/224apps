c6a83722cb295874ec43845a255980fecdfa3cdb
224Apps <224apps@gmail.com>
2022-05-11 13:15:33 -0400

    Move `RemoteFeedLoader` components to production.

---
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -0,0 +1,26 @@
+//
+//  RemoteFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Abdoulaye Diallo on 5/11/22.
+//
+
+import Foundation
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteFeedLoader {
+    private let client: HTTPClient
+    private let url: URL
+    
+    public init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load(){
+        client.get(from: url)
+    }
+}
