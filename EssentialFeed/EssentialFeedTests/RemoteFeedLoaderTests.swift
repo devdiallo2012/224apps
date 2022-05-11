fbcdc3f0c43b621079508d2c200ef04ee2d5f02f
224Apps <224apps@gmail.com>
2022-05-11 14:01:51 -0400

    Simplify `RemoteFeedLoader.HTTPClientSpy`

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -16,7 +16,7 @@ class RemoteFeedLoaderTests: XCTestCase {
 
         _ = RemoteFeedLoader(client: client, url: url)
         
-        XCTAssertNil(client.requestedURL )
+        XCTAssertTrue(client.requestedURLs.isEmpty )
     }
     
     func test_load_requestsDataFromURL(){
@@ -25,7 +25,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         
         sut.load()
         
-        XCTAssertEqual(client.requestedURL, url)
+        XCTAssertEqual(client.requestedURLs, [url])
     }
     
     func test_loadTwice_requestsDataFromURLTwice(){
@@ -48,11 +48,9 @@ class RemoteFeedLoaderTests: XCTestCase {
     }
     
     private class HTTPClientSpy: HTTPClient {
-        var requestedURL: URL?
         var requestedURLs = [URL]()
         
          func get(from url: URL) {
-            requestedURL = url
              requestedURLs.append(url)
         }
     }
