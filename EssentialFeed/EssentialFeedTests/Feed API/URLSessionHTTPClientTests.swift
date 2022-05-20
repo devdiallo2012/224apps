d5a643a93225edb1490a4eb150821355f38d8c45
224Apps <224apps@gmail.com>
2022-05-20 13:06:44 -0400

    Moved the `requestedObserver ` to the startLoading method

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -155,7 +155,6 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
         
@@ -164,6 +163,11 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
+            
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                requestObserver(request)
+            }
           
             if let data = URLProtocolStub.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
