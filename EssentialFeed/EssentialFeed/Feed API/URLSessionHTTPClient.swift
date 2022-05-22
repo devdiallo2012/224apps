a298188c15154b07060b0a1cc70dcb7e4519fc5a
224Apps <224apps@gmail.com>
2022-05-22 07:07:17 -0400

    Subclass  `URLSessionHTTPClient` to `HTTPClient`

--- EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
+++ EssentialFeed/EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-public class URLSessionHTTPClient {
+public class URLSessionHTTPClient: HTTPClient {
     private let session: URLSession
     
     public init(session:  URLSession = .shared){
