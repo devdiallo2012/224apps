e243081696e4f985f24a4fe8b3a56243abf19c80
224Apps <224apps@gmail.com>
2022-06-07 13:09:07 -0400

    Update the memory leak tracking of the SUT

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -138,9 +138,9 @@ class CodableFeedStoreTests: XCTestCase {
     
     //MARK: - Helpers
     
-    private func makeSUT() -> CodableFeedStore {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
         let sut = CodableFeedStore(storeURL: testSpecificStoreURL())
-        trackMemoryLeaks(sut)
+        trackMemoryLeaks(sut, file: file, line: line)
         return sut
     }
     
